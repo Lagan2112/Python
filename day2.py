c4192753444094cf950064a3aea92f44a9ac8b49
Lagan2112 <lagan8558.beaift25@chitkara.edu.in>
2025-08-21 15:54:06 +0530

    Add files via upload

---
+++ day2.py
@@ -0,0 +1,11 @@
+a = int(input("enter the no"))
+if(a>=90 and  a<=100):
+    print("A")
+elif(a<90 and a>=75):
+    print("B")
+elif(a<75 and a>=60):
+    print("c")
+elif(a<60 and a>=0):
+    print("fail")
+else:
+    ("invalid")

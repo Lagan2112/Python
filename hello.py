8e35f9e80c42c27ef389df80005927cae60a1f07
Lagan2112 <lagan8558.beaift25@chitkara.edu.in>
2025-08-21 15:55:33 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,22 @@
+a = 10 
+b = 20 
+c = a+b
+print(c)
+
+a = 10.7
+b = 20.3
+c = a+b
+print(c)
+
+a = 8788787989
+b = 266779
+c = a+b
+print(c)
+
+a = "k"
+b = "o"
+c = a+b
+print(c)
+
+name = input("naam likho:")
+print(name)

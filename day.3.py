22c19290d4723a31828ff2ef5551a6bcd739a16a
Lagan2112 <lagan8558.beaift25@chitkara.edu.in>
2025-08-26 10:54:59 +0530

    Add files via upload

---
+++ day.3.py
@@ -0,0 +1,10 @@
+# for i in range(1, 100):
+#     print("numbers:",i)
+
+# fruits = ["aam","apple","watermelon"]
+# for d in fruits:
+#     print("i like:",d)
+i = 1
+while i<=6:
+    print("number:",i)
+    i+=1

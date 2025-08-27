e6fe623cfe12c73bd7b6bc79043d830255abf3b3
Lagan2112 <lagan8558.beaift25@chitkara.edu.in>
2025-08-27 09:07:51 +0530

    Add files via upload

---
+++ hw2.py
@@ -0,0 +1,22 @@
+# a = int(input("enter a no :"))
+# b = int(input("enter a no :"))
+# c = a+b
+# print("sum of no is :",(c))
+
+
+# a = int(input("enter a no :"))
+# b = int(input("enter a no :"))
+# c = a -b 
+# print("diff of no is",c)
+
+
+# a = int(input("enter a no :"))
+# b = int(input("enter a no :"))
+# c = a*b
+# print("mulp of no is",c)
+
+
+# a = int(input("enter a no :"))
+# b = int(input("enter a no :"))
+# c =a/b
+# print("divison is : ",c )

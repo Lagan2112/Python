175c63e890d9140ec3d37ca17b77733368599b4a
Lagan2112 <lagan8558.beaift25@chitkara.edu.in>
2025-08-27 10:48:16 +0530

    Add files via upload

---
+++ SWAP.py
@@ -0,0 +1,17 @@
+# a =5
+# b = 10
+# print("before swapping a = ",a,"b =",b)
+
+# a = a+b
+# b = a-b
+# a = a-b
+# print("after swapping a = ",a,"b=",b)
+a =5
+b = 10
+print("before swapping a = ",a,"b =",b)
+
+
+c= a+b
+b =c-b
+a =c-a
+print("after swap a =",a, "b=",b)
---
+++ sumnnatural.no.py
@@ -0,0 +1,8 @@
+# n = int(input("enter no"))
+# print(n*(n+1)/2)
+
+n =5
+sum = 0
+for i in range(0,n+1):
+    sum+=i
+print(sum)

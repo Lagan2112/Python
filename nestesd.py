3754c9f745a3b2047e103f14a798b6749a33af57
Lagan2112 <lagan8558.beaift25@chitkara.edu.in>
2025-08-28 11:14:06 +0530

    Add files via upload

---
+++ nestesd.py
@@ -0,0 +1,8 @@
+print("day5")
+print("Today topic in nested looping")
+
+
+for i in range(1,4):
+    for j in range(1,4):
+        print(i*j,end=" ")
+    print()
---
+++ pattern.py
@@ -0,0 +1,37 @@
+# n = 5 
+# for i in range (1,n+1):
+#     for j in range(i):
+#         print("1",end="  ")
+#     print()
+
+
+# n = 5 
+# for i in range (0,n+1):
+#     for j in range(i):
+#         print(i+1, end = " ")
+#     print()
+    
+
+rows =5 
+num = 1
+for i in range(1,rows+1):
+    for j in range(i):
+        print(num,end=" ")
+        num+=1
+    print()
+
+
+# n =5
+# for i in range (0,n+1):
+#     for j in range(i):
+#         print("", end = " ")
+#     print()
+    
+
+
+
+
+    
+
+
+
